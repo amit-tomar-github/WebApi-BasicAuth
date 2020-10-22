74ffdeabc1dca8e0f99d8b8541b972f9b70a7358
Amit Tomar <theamittomar@gmail.com>
2020-10-22 12:38:06 +0530

    Add project files.

---
+++ AEPApi/Controllers/ServiceController.cs
@@ -0,0 +1,50 @@
+﻿using AEPApi.Dal;
+using AEPApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+
+namespace AEPApi.Controllers
+{
+    [BasicAuthentication]
+    public class ServiceController : ApiController
+    {
+        //without parameter
+        [HttpGet]
+        public string GetTestValue()
+        {
+            return "Working";
+        }
+        //with default url parameter
+        public string GetValue(string id)
+        {
+            return $"Hello {id} , how are you";
+        }
+        public List<Part> GetValue()
+        {
+            List<Part> parts = new List<Part>();
+            parts.Add(new Part
+            {
+                BackNo = "b100", Description = "Test"
+            });
+            parts.Add(new Part
+            {
+                BackNo = "b200",
+                Description = "Test2"
+            });
+            return parts;
+        }
+        //customer route here {} is mandatory
+        [Route("api/getname/{name}")]
+        public string GetMyName(string name)
+        {
+            return $"Hello {name} , how are you";
+        }
+
+    }
+}
